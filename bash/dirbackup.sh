89655cbbb803101279726e504643deec21f78ff5
Richard Cave <rich.cave@gmail.com>
2015-03-01 18:12:06 -0800

    Updated dirbackup and manhog bash files

--- bash/dirbackup.sh
+++ bash/dirbackup.sh
@@ -1,24 +1,38 @@
-#!/bin/bash
+#!/bin/sh
 
+BACKUP_DIR=""
 TODAY=`date +%Y-%m-%d`
 USER=`whoami`
 
+
 # get args
-while getopts "d:" Option
-do
-  case $Option in
-    d ) BACKUP_DIR="$OPTARG";;
-    [?] ) echo >&2 "Usage: $0 [-d database] [-f] [-u user] [-t type] ..."
-         exit 1;;
+while getopts ":d:" opt; do
+  case $opt in
+    d ) 
+         BACKUP_DIR="$OPTARG"
+         ;;
+    \? ) 
+         echo >&2 "Usage: $0 [-d directory]"
+         exit 1
+         ;;
+    :) 
+         echo "Option -$OPTARG requires an argument." >&2
+         exit 1
+         ;;
   esac
 done
 
-shift $(($OPTIND - 1))
+if [ $BACKUP_DIR == "" ]; then
+  echo "No directory specified!"
+  exit 1
+fi
 
 if [ ! -d $BACKUP_DIR ]; then
   echo "${BACKUP_DIR}:  No such directory!"
   exit 1
 else
-  # zip
-  /bin/tar -zcvf /home/${USER}${BACKUP_DIR}-${TODAY}.tar.gz ${BACKUP_DIR}/
+  # get basename
+  BASENAME=`/usr/bin/basename ${BACKUP_DIR}`
+  # gzip directory
+  /usr/bin/tar -zcvf ${BASENAME}-${TODAY}.tar.gz -C ${BACKUP_DIR} ${BACKUP_DIR}
 fi
--- bash/manhog.sh
+++ bash/manhog.sh
@@ -1,2 +1,7 @@
-#!/bin/bash
-find /usr/share/man/ -type f -ls | sort -k 7 -r -n | head -n 1 | awk -F' ' '{print $11}' | sed 's/.*\///'
+#!/bin/sh
+
+MAN_DIR=/usr/share/man
+ABS_FILE=`find ${MAN_DIR} -type f -ls | sort -k 7 -r -n | head -n 1 | awk -F' ' '{print $11}'`
+#echo ${ABS_FILE}
+FILE=`/usr/bin/basename ${ABS_FILE}`
+echo "Largest man page: ${FILE}"
